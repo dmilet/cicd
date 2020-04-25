c59f81957e125be435946d4c028cc4c076f838eb
David Milet <david.milet@gmail.com>
2020-04-25 17:58:36 -0400

    fix script

--- images/buildah/app/buildah_bud.sh
+++ images/buildah/app/buildah_bud.sh
@@ -6,8 +6,6 @@ BUILDAH_CMD="buildah $STORAGE"
 # $1 : full image name
 # $2 : Dockerfile
 # $3 : build context
-container=$($BUILDAH_CMD bud --squash --tag $1 -f $2 $2)
-
-#container=$($BUILDAH_CMD from $1)
+$BUILDAH_CMD bud --squash --tag $1 -f $2 $3
+container=$($BUILDAH_CMD from $1)
 $BUILDAH_CMD commit -f docker $container $1
-
